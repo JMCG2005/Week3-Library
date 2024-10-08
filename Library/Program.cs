0d17a6aababb2e37caa53733352465f7388e41df
Jack McGill <McGill-J9@ulster.ac.uk>
2024-10-08 10:33:37 +0100

    Add project files.

---
+++ Library/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+
+class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+    int NoOfpages;
+
+    void DisplayInfo()
+    {
+        //Output the book information to the console
+        Console.WriteLine("book information");
+        Console.WriteLine("------------------");
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine($"Book NoOfpages: {NoOfpages}");
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        //create a new instance of the book class
+        Book book = new Book();
+        book.Title = "C# for beginners";
+        book.Author = "Bill Gates";
+        book.ISBN = "123456677";
+        book.NoOfpages = 200;
+
+        Book book2 = new Book();
+        book2.Title = "c# methods and classes";
+        book2.Author = "Micrsoft";
+        book2.ISBN = "625618126";
+        book2.NoOfpages = 50;
+
+        //output book information to the console
+        book.DisplayInfo();
+        book2.DisplayInfo();
+
+    }
+}
